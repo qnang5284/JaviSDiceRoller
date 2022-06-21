bce6bfce6f22890f0582d8d911fd5d69fc1e17ec
JaviS-Linux <qnang5284@gmail.com>
2022-06-21 22:56:04 +0200

    fix buttons show roll

--- assets/scripts/diceroller.js
+++ assets/scripts/diceroller.js
@@ -1,74 +1,108 @@
 function getUserInput() {
+  const dice = parseInt(numDice.value);
+  const nDice = isNaN(dice) ? 1 : dice;
+  const modi = parseInt(modifier.value);
+  const mod = isNaN(modi) ? 0 : modi;
+  
   return { 
-    numDice: numDie.value, 
-    dieFaces: customDieFaces.value, 
-    rollType: rollType.value
+    numDice: nDice, 
+    dieFaces: parseInt(customDieFaces.value),
+    modifier: mod
   };
 }
 
-function rollD100() {
-  const userInput = getUserInput();
-  const result = makeRoll(userInput.numDice, 100, userInput.rollType);
-  return result;
-}
-d100.addEventListener('click', rollD100);
-
-function rollD20() {
-  const userInput = getUserInput();
-  const result = makeRoll(userInput.numDice, 20, userInput.rollType);
-  return result;
-}
-d20.addEventListener('click', rollD20);
-
-function rollD12() {
-  const userInput = getUserInput();
-  const result = makeRoll(userInput.numDice, 12, userInput.rollType);
-  return result;
-}
-d12.addEventListener('click', rollD12);
-
-function rollD10() {
-  const userInput = getUserInput();
-  const result = makeRoll(userInput.numDice, 10, userInput.rollType);
-  return result;
-}
-d10.addEventListener('click', rollD10);
-
-function rollD8() {
-  const userInput = getUserInput();
-  const result = makeRoll(userInput.numDice, 8, userInput.rollType);
-  return result;
-}
-d8.addEventListener('click', rollD8);
-
-function rollD6() {
-  const userInput = getUserInput();
-  const result = makeRoll(userInput.numDice, 6, userInput.rollType);
-  return result;
-}
-d6.addEventListener('click', rollD6);
-
-function rollD4() {
-  const userInput = getUserInput();
-  const result = makeRoll(userInput.numDice, 4, userInput.rollType);
-  return result;
-}
-d4.addEventListener('click', rollD4);
-
-function rolrollCustomDice() {
-  const userInput = getUserInput();
-  const result = makeRoll(userInput.numDice, userInput.numFaces, userInput.rollType);
-  return result;
-}
-customDie.addEventListener('click', rollCustomDice);
 
 /**
- * Returns a random number.
- * @param {*} numFaces
- * @returns random number between 1 and numFaces
+ * Rolls D100
  */
-function randomNumber(numFaces) {
-  return Math.floor(Math.random() * numFaces) + 1;
+function rollD100() {
+  customRoll(100);
+}
+d100Button.addEventListener('click', rollD100);
+
+/**
+ * Rolls D20
+ */
+function rollD20() {
+  customRoll(20);
+}
+d20Button.addEventListener('click', rollD20);
+
+/**
+ * Rolls D12
+ */
+function rollD12() {
+  customRoll(12);
+}
+d12Button.addEventListener('click', rollD12);
+
+/**
+ * Rolls D10
+ */
+function rollD10() {
+  customRoll(10);
+}
+d10Button.addEventListener('click', rollD10);
+
+/**
+ * Rolls D8
+ */
+function rollD8() {
+  customRoll(8);
+}
+d8Button.addEventListener('click', rollD8);
+
+/**
+ * Rolls D6
+ */
+function rollD6() {
+  customRoll(6);
+}
+d6Button.addEventListener('click', rollD6);
+
+/**
+ * Rolls D4
+ */
+function rollD4() {
+  customRoll(4);
+}
+d4Button.addEventListener('click', rollD4);
+
+function rollCustomDice() {
+  customRoll();
+}
+customDieButton.addEventListener('click', rollCustomDice);
+
+/**
+ * Basic roll, then prints results.
+ * @param {*} dieFaces when it's !== 0, it will replace the user's entry
+ */
+function customRoll(dieFaces = 0) {
+  let userInput = getUserInput();
+  if (dieFaces !== 0) {
+    userInput.dieFaces = dieFaces;
+  }
+  if (!isNaN(userInput.dieFaces)) {
+    const result = makeRoll(userInput.numDice, userInput.dieFaces, userInput.modifier);
+    writeResult(userInput, result);
+  }
+}
+
+/**
+ * Shows result
+ * @param {*} result array of integers
+ */
+function writeResult (userInput, result) {
+  let rollDesc = "Rolling " + userInput.numDice + "D" + userInput.dieFaces;
+  if (userInput.modifier > 0) {
+    rollDesc += " + " + userInput.modifier;
+  } else if (userInput.modifier < 0) {
+    rollDesc += " - " + userInput.modifier*-1;
+  }
+  currentRoll.textContent = rollDesc;
+  for (const res of result) {
+    console.log(res);
+  }
 }
 
 /**
@@ -79,18 +113,19 @@ function randomNumber(numFaces) {
  * other than normal with numfaces != 20 or if rollType
  * is different than 0, 1 or 2.
  * 
- * @param {*} numDie number of dice.
+ * @param {*} numDice number of dice.
  * @param {*} numFaces number of faces of each die.
+ * @param {*} modifier to add to the total roll
  * @param {*} rollType 0 if it's a normal roll (default),
  *          otherwise it is asumed advantage/disadvantage.
  * @returns and array of integers or null
  */
-function makeRoll(numDie, numFaces, rollType = 0) {
-  let result;
+ function makeRoll(numDice, numFaces, modifier = 0, rollType = 0) {
+  let result = [];
   if (rollType>=0 && rollType<=2) {
     if (rollType === 0) {
-        for (let i = 0; i < numDie; i++) {
-        result[i] = randomNumber(numFaces);
+        for (let i = 0; i < numDice; i++) {
+        result[i] = randomNumber(numFaces) + modifier;
         }
     } else {
         // Advantage / disadvantage
@@ -106,4 +141,13 @@ function makeRoll(numDie, numFaces, rollType = 0) {
   }
 
   return result;
+}
+
+/**
+ * Returns a random number.
+ * @param {*} numFaces
+ * @returns random number between 1 and numFaces
+ */
+ function randomNumber(numFaces) {
+  return Math.floor(Math.random() * numFaces) + 1;
 }
--- assets/scripts/dicey-in-out.js
+++ assets/scripts/dicey-in-out.js
@@ -1,19 +1,18 @@
 // Roll definition
-const numDice = document.getElementById("dice-num-input");
+const numDice = document.getElementById("number-of-dice");
 const modifier = document.getElementById("modifier");
-const rollType = getRollType();
 
 // Dice buttons
-const d100 = document.getElementById("btn-d100");
-const d20 = document.getElementById("btn-d20");
-const d12 = document.getElementById("btn-d12");
-const d10 = document.getElementById("btn-d10");
-const d8 = document.getElementById("btn-d8");
-const d6 = document.getElementById("btn-d6");
-const d4 = document.getElementById("btn-d4");
+const d100Button = document.getElementById("btn-d100");
+const d20Button = document.getElementById("btn-d20");
+const d12Button = document.getElementById("btn-d12");
+const d10Button = document.getElementById("btn-d10");
+const d8Button = document.getElementById("btn-d8");
+const d6Button = document.getElementById("btn-d6");
+const d4Button = document.getElementById("btn-d4");
 
 // Custom dice
-const customDie = document.getElementById("btn-custom");
+const customDieButton = document.getElementById("btn-custom");
 const customDieFaces = document.getElementById("num-faces");
 
 // Logs
@@ -21,23 +20,3 @@ const currentRoll = document.getElementById("current-roll");
 const currentResult = document.getElementById("current-result");
 const currentTotalResul = document.getElementById("total-result");
 const rollLog = document.getElementById("roll-text-log");
-
-/**
- * Returns if we're working with a normal roll (0, default), 
- * rolling with advantage (1) or rolling with disadvantage (2)
- * @returns 0, 1, or 2
- */
-function getRollType() {
-  var rollType = 0;
-  const normalRoll = document.getElementById("normal-roll");
-  const advantage = document.getElementById("adv-roll");
-  const disadvantage = document.getElementById("disadv-roll");
-  if (disadvantage.checked) {
-    rollType = 2;
-  } else if (advantage.checked) {
-    rollType = 1;
-  } else if (normalRoll.checked) {
-    rollType = 0;
-  }
-  return rollType;
-}
